4032eefc4e155036c71d8a7651cbafb98b00ba96
UjjwalSilwal <124781352+UjjwalSilwal@users.noreply.github.com>
2024-03-31 18:30:45 +0545

    initaling async handler for user controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,1 +1,9 @@
 // Once we define this We are able to create a user delete a user
+
+import express from "express";
+
+const router = express.Router()
+
+router.route('/').post(createUser)
+
+export default router
