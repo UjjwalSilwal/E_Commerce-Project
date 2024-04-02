23b174dc24842609fd5eebdf825042412b9884b4
UjjwalSilwal <124781352+UjjwalSilwal@users.noreply.github.com>
2024-04-02 10:24:08 +0545

    problem solved by hashed password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -107,7 +107,9 @@ const updateCurrentUserProfile = asyncHandler(async(req, res) => {
         user.email = req.body.email || user.email
 
         if (req.body.password) {
-            user.password = req.body.password
+            const salt = await bcrypt.genSalt(15)
+            const hashedPassword = await bcrypt.hash(req.body.password,salt)
+            user.password = hashedPassword
         }
 
         const updatedUser = await user.save()
