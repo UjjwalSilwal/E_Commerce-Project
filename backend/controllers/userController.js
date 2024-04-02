e77ac19a3457fc150369e94f7fcca3baa30d12be
UjjwalSilwal <124781352+UjjwalSilwal@users.noreply.github.com>
2024-04-02 09:47:18 +0545

    Making User Profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -84,5 +84,20 @@ const getAllUsers = asyncHandler(async(req, res) => {
     res.json(users)
 })
 
+const getCurrentUserProfile =  asyncHandler(async(req, res) => {
+    const user= await User.findById(req.user._id)
 
-export { createUser,loginUser,logoutCurrentUser,getAllUsers };
+    if (user) {
+        res.json({
+            _id: user._id,
+            username: user.username,
+            email: user.email
+        })
+    } else {
+        res.status(404)
+        throw new Error("User not found")
+    }
+})
+
+
+export { createUser,loginUser,logoutCurrentUser,getAllUsers,getCurrentUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,18 @@
 // Once we define this We are able to create a user delete a user
 
 import express from "express";
-import { createUser, loginUser, logoutCurrentUser ,getAllUsers} from "../controllers/userController.js";
+import { createUser, loginUser, logoutCurrentUser ,getAllUsers,getCurrentUserProfile} from "../controllers/userController.js";
 import { authenticate,authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router()
 
 // For create, authenticate, authorizeAdmin user
-router.route("/").post(createUser).get(authenticate,getAllUsers)
+router.route("/").post(createUser).get(authenticate,authorizeAdmin,getAllUsers)
 
 // For log-in the user
 router.post('/auth', loginUser)
-router.post('/logout',logoutCurrentUser)
+router.post('/logout', logoutCurrentUser)
+
+router.route('/profile').get(authenticate,getCurrentUserProfile)
 
 export default router;
