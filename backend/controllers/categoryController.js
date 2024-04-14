a3d7151c08df849eb69cb546323707e3bc34e5e4
Ujjwal Silwal <ujjwalsilwal321@gmail.com>
2024-04-14 16:45:01 +0545

    category database created with unique id to save the items and we cannot recreate the same two items

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -4,7 +4,21 @@ import asyncHandler from "../middlewares/asyncHandler.js"
 const createCategory = asyncHandler(async(req, res)=>{
     try {
         const {name} = req.body
-        console.log(name)
+//The trim method in JavaScript is a built-in string method that removes whitespace characters from the beginning and end of a string.
+
+        if(!name){
+            return res.json({error : "Name is required"})
+        }
+        // Check for the existing category name and if it is already available
+        const existingCategory = await Category.findOne({name})
+
+        if(existingCategory){
+            return res.json({error: "Already esists"})
+        }
+
+        const category = await new Category({name}).save()
+        res.json(category)
+
     } catch (error) {
         console.log(error)
         return res.status(400).json(error)
