36782a679384fccc9e6ed075ccc6e430fa390016
Ujjwal Silwal <ujjwalsilwal321@gmail.com>
2024-04-14 17:13:57 +0545

    category update completed

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
 const router = express.Router()
-import { createCategory } from "../controllers/categoryController.js";
+import { createCategory,
+        updateCategory
+ } from "../controllers/categoryController.js";
 
 // i need authentication and authorization for my middleware
 import {authenticate, authorizeAdmin} from '../middlewares/authMiddleware.js'
 
-router.route("/").post(createCategory)
+// user should be authenticate and authorize as a Admin to create category
+router.route("/").post(authenticate,authorizeAdmin, createCategory)
+
+// updating the category
+router.route("/:categoryId").put(authenticate,authorizeAdmin, updateCategory)
 
 export default router 
