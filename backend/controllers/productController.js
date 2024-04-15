70bd4168e120c10f23a8f3832a14cfa05494f725
Ujjwal Silwal <ujjwalsilwal321@gmail.com>
2024-04-15 16:27:45 +0545

    creating a product json

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,48 @@ import asyncHandler from "../middlewares/asyncHandler.js";
 import Product from "../models/productModel.js";
 
 const addProduct = asyncHandler(async(req,res)=>{
-    res.send('hello')
+    try {
+        const {name, description, price, category, quantity, brand} = req.fields
+        // console.log(name)
+        // console.log(description)
+        // console.log(price)
+        // console.log(category)
+        // console.log(quantity)
+        // console.log(brand)
+        // go to postman : body : form-data
+
+        // Validation
+
+        switch(true){
+            case !name:
+                return res.json({error:"Name is required"})
+
+            case !description:
+                return res.json({error:"Description is required"})
+
+            case !price:
+                return res.json({error:"Price is required"})
+
+            case !category:
+                return res.json({error:"Category is required"})
+                
+            case !quantity:
+                return res.json({error:"Quantity is required"})
+
+            case !brand:
+                return res.json({error:"Brand is required"})
+
+        }  
+        
+        const product = new Product({...req.fields})
+        await product.save()
+        res.json(product)
+
+    } catch (error) {
+        console.error(error)
+        res.status(400).json(error.message)
+    }
+
 })
 
 export {addProduct}
