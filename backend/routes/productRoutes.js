ae2800c678faa8464e9f3ba517cc17b801fcce1d
Ujjwal Silwal <ujjwalsilwal321@gmail.com>
2024-04-15 16:46:28 +0545

    updating product completed

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,12 +3,17 @@ import formidable from "express-formidable"
 const router = express.Router()
 
 // controllers
-import { addProduct } from '../controllers/productController.js'
+import { addProduct ,
+        updateProductDetails, 
+} from '../controllers/productController.js'
 
 import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js'
 import checkId from '../middlewares/checkId.js'
 
 // formiddable : because we are going to work with the FORM data
 router.route('/').post(authenticate, authorizeAdmin,formidable(), addProduct)
+
+// for updating the product
+router.route('/:id').put(authenticate,authorizeAdmin,formidable(), updateProductDetails)
 
 export default router
