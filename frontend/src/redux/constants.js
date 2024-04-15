00372ae52779610d6de19b1f6f7a93da87d24ee8
Ujjwal Silwal <ujjwalsilwal321@gmail.com>
2024-04-15 21:17:20 +0545

    adding productapi slice

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,3 +1,5 @@
 export const BASE_URL = ''   //We are going to Use the proxy thats why its empty
 export const USERS_URL = "/api/users"
 export const CATEGORY_URL = "/api/category"
+export const PRODUCT_URL = '/api/products'
+export const UPLOAD_URL = '/api/upload'
