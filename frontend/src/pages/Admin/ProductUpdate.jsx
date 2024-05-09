0e15e175f18ecd747e3bff72fd2065eb934bda6d
Ujjwal Silwal <ujjwalsilwal321@gmail.com>
2024-05-09 19:13:29 +0545

    updated

--- frontend/src/pages/Admin/ProductUpdate.jsx
+++ frontend/src/pages/Admin/ProductUpdate.jsx
@@ -40,7 +40,9 @@ const ProductUpdate = () => {
             setQuantity(productData.setQuantity)
             setBrand(productData.brand)
             setImage(productData.image)
-        })
+        }
+    },[productData])
+
   return (
     <div>ProductUpdate</div>
   )
