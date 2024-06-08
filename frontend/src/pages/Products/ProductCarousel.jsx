f25bd9ba1bc200fae3f0447339b34b18dbf2577f
Ujjwal Silwal <ujjwalsilwal321@gmail.com>
2024-06-08 20:41:33 +0545

    env

--- frontend/src/pages/Products/ProductCarousel.jsx
+++ frontend/src/pages/Products/ProductCarousel.jsx
@@ -27,6 +27,7 @@ const ProductCarousel = () => {
         autoplayspeed:3000,
     }
 
+
   return (
     <div className="mb-4 xl:block lg:block md:block">
         {isLoading ? null : error ? (
@@ -38,6 +39,31 @@ const ProductCarousel = () => {
                 products.map(({image, _id, price, description, brand, createdAt, numReviews, rating, quantity, countInStock })=>(
                     <div key={_id}>
                         <img src={image} alt={name} className="w-full rounded-lg object-cover h-[30rem]"  />
+
+                        <div className="flex justify-between w-[20rem]">
+                            <div className="one">
+                                <h2>{name}</h2>
+                                <p>Rs {price}</p> <br />  <br />
+                                <p className="w-[25rem]">{description.substring(0,170)}...</p>
+                            </div>
+
+                            <div className="flex justify-between w-[20rem]">
+                                <div className="one">
+                                    <h1 className="flex items-center mb-6 w-[8rem]">
+                                        <FaStore className="mr-2 text-white" /> Brand:{brand}
+                                    </h1>
+                                    <h1 className="flex items-center mb-15 w-[15rem]">
+                                        <FaStore className="mr-2 text-white" /> Added:{" "}{moment(createdAt).fromNow()}
+                                    </h1>
+                                    <h1 className="flex items-center mb-6 w-[8rem]">
+                                        <FaStore className="mr-2 text-white" /> Reviews:{numReviews}
+                                    </h1>
+                                </div>
+
+                                
+                            </div>
+
+                        </div>
                     </div>
                 ))
             }
